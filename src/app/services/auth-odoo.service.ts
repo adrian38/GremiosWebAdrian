0e2d4a73417d41fc126d1c8aa1ca263e3b8a66d2
Adrian <131995adrian@gmail.com>
2020-11-09 19:09:19 -0500

    LibMigration
    auth_odoo

--- src/app/services/auth-odoo.service.ts
+++ src/app/services/auth-odoo.service.ts
@@ -1,7 +1,16 @@
 import { Injectable } from '@angular/core';
 import * as odoo_xmlrpc from 'odoo-xmlrpc'
-import {UsuarioModel} from '../models/usuario.model'
-import {Observable, Subject} from 'rxjs'
+import { UsuarioModel } from '../models/usuario.model'
+import { Observable, Subject } from 'rxjs'
+let jayson = require('../../../node_modules/jayson/lib/client/');
+const jaysonServer = {
+  host: '192.168.0.106',
+  port: '8069',
+  db: 'demo',
+  username: '',
+  password: '',
+  pathConnection: '/jsonrpc'
+}
 
 let odooClient = new odoo_xmlrpc({
   url: 'http://' + '192.168.0.106',
@@ -12,77 +21,91 @@ let odooClient = new odoo_xmlrpc({
 });
 
 let user$ = new Subject<UsuarioModel>();
-let connected: boolean=false;
-let userLogin : UsuarioModel = new UsuarioModel();
+let connected: boolean = false;
+let userLogin: UsuarioModel = new UsuarioModel();
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthOdooService {
 
-  userType:string="";
+  userType: string = "";
 
-public OdooInfo = odooClient;
+  public OdooInfo = odooClient;
 
   constructor() {
 
   }
 
 
-  login(usuario:UsuarioModel):void{
+  login(usuario: UsuarioModel): void {
 
     odooClient.username = usuario.username;
     odooClient.password = usuario.password;
 
-    let get_user = function(id:number) {
+    jaysonServer.username = usuario.username;
+    jaysonServer.password = usuario.password;
+
+    let get_user = function (id: number) {
       let inParams = []
-      inParams.push([id])
-      inParams.push(['partner_id'])
+      inParams.push([['id', '=', id]])
+      inParams.push([
+        'name',
+        'login',
+        'email',
+        'partner_id',
+        'groups_id',
+        'image_1920',
+        'classification'
+      ])
       let params = []
       params.push(inParams)
-      odooClient.execute_kw('res.users', 'read', params, function (err, value) {
-          if (err) {
-              console.log(err);
-          } else {
-              usuario.partner_id = value[0].partner_id[0];
-              usuario.realname = value[0].partner_id[1];
-              get_type_user(value[0]['partner_id'][0]);
-          }
-      })
+
+      let fparams = [];
+      fparams.push(jaysonServer.db);
+      fparams.push(id);
+      fparams.push(jaysonServer.password);
+      fparams.push('res.users');//model
+      fparams.push('search_read');//method
+
+      for (let i = 0; i < params.length; i++) {
+        fparams.push(params[i]);
+      }
+
+      client.request('call', { service: 'object', method: 'execute_kw', args: fparams }, function (err, error, value) {
+        if (err) {
+          console.log(err);
+        } else {
+
+          usuario.partner_id = value[0].partner_id[0];
+          usuario.realname = value[0].name;
+
+          /*             if(value[0].classification === "custumer")
+                        {
+                          usuario.type = "client"
+                          console.log("cliente");
+          
+                        } else if(value[0].classification === "vendor")
+                        {
+                          usuario.type = "provider"
+                          console.log("proveedor");
+                        } */
+
+          usuario.type = "client"
+          user$.next(userLogin);
+        }
+      });
     }
+    const client = jayson.http({ host: jaysonServer.host, port: jaysonServer.port + jaysonServer.pathConnection });
+    client.request('call', { service: 'common', method: 'login', args: [jaysonServer.db, jaysonServer.username, jaysonServer.password] }, function (err, error, value) {
 
-    let get_type_user = function(id:number) {
-      let inParams = []
-      inParams.push([id])
-      inParams.push(['supplier_rank', 'customer_rank'])
-      let params = []
-      params.push(inParams)
-      odooClient.execute_kw('res.partner', 'read', params, function (err, value) {
-          if (err) {
-              console.log(err);
-          } else {
-              if(value[0].supplier_rank > 0)
-              {
-                usuario.type = "provider"
-
-              } else if(value[0].customer_rank > 0)
-              {
-                usuario.type = "client"
-              }
-              user$.next(userLogin);
-          }
-      })
-    }
-
-
-    odooClient.connect(function (err, value){
-      if (err) {
+      if (err || !value) {
         console.log("Login Failed");
         console.log(err);
         usuario.connected = false;
         connected = usuario.connected;
       } else {
-        console.log("Login Success");
+        console.log("Connected");
         usuario.connected = true;
         usuario.id = value;
         connected = usuario.connected;
@@ -90,13 +113,18 @@ public OdooInfo = odooClient;
         get_user(usuario.id);
       }
     });
+
+
+
+
+
   }
 
-  getUser$(): Observable<UsuarioModel>{
+  getUser$(): Observable<UsuarioModel> {
     return user$.asObservable();
   }
 
-  getUser(): UsuarioModel{
+  getUser(): UsuarioModel {
     return userLogin;
   }
 
